3df6c613343f141c7095425ef5134482d5225167
JanithMadura <47352490+JanithMadura@users.noreply.github.com>
2019-08-13 22:31:18 +0530

    sx
    databases were created using phpMyAdmin . So the sample of the database added by naming as users.pdf.

---
+++ includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+$dbServername="localhost:3307";
+$dbUsername="root";
+$dbPassword="";
+$dbName="logsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
---
+++ users.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Aug 13, 2019 at 04:32 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_first` varchar(256) NOT NULL,
+  `user_last` varchar(256) NOT NULL,
+  `user_email` varchar(256) NOT NULL,
+  `user_uid` varchar(256) NOT NULL,
+  `user_pwd` varchar(256) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_first`, `user_last`, `user_email`, `user_uid`, `user_pwd`) VALUES
+(1, 'Janith', 'Rathnayake', 'rathnayakejm@gmail.com', 'Janith', '$2y$10$/OyhZe4OR95BkW8v7IzSueGtncjqzqfurf3fOq975q1kddytMEaey'),
+(2, 'dilan', 'Rathnayake', 'dilanrath@gmail.com', 'Dilan', '$2y$10$EW9lS189p2jKuPgj7VnH6uf19YDcILLuHLQYf8yWUbldCoa.qZXTK');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
